26271cf5fb5a456526a24905e2281b64d66f1573
mark <markkbrown1@gmail.com>
2017-04-20 01:41:13 -0400

    added htmls

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+// empty friends array to store new entries.
+var friends = [{
+
+}
+];
+
+module.exports=friends;
---
+++ server.js
@@ -0,0 +1,28 @@
+// declare basic npm values.
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Sets up the Express App
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+// Routes mapping
+require('./app/routing/api-routes.js');
+require('./app/routing/html-routes.js');
+
+// starts the function server listener.
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
